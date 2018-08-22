e3ac10b4ee9500897928769f1c4f46547a3331c1
albert_chen <albert_chen@trend.com.tw>
2018-08-22 11:07:55 +0800

    add player info

--- src/bot.py
+++ src/bot.py
@@ -1,3 +1,5 @@
+# utf-8
+
 import gym
 import universe  # register the universe environments
 import sys
@@ -16,3 +18,18 @@ class TrendBotBase(BotBase):
     def declare_action(self, player_obs, table_obs):
         raise NotImplemented()
 
+
+class Player:
+    """
+    [S, H, D, C]
+    """
+
+    def __init__(self):
+        self.a = [None, None, None, None]
+        self.b = self._init_card() #過去出過的牌
+        self.c = self._init_card() # 每個人收的
+        self.d = self._init_card() # 已知的牌，換的
+        self.f = self._init_card() # 缺門
+
+    def _init_card(self):
+        return [[-1 for i in range(13)] for i in range(3)]
