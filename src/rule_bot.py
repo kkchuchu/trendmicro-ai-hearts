a6150d507fa0934b05a9fc00957e211116b78d57
ChunTing Wu <chunting_wu@trendmicro.com>
2018-08-28 14:05:14 +0800

    Fix chunting

--- src/rule_bot.py
+++ src/rule_bot.py
@@ -85,6 +85,7 @@ class GameInfo:
         return max_rank
 
     def get_possiable_min(self, n):
+        # 非最後一手
         card = None
 
         if self.table.first_draw:
@@ -111,6 +112,9 @@ class GameInfo:
                 if less_count <= n and less_count >= max_less:
                     max_less = less_count
                     max_target = '%s%s' % (INT_TO_RANK[r], s)
+                    system_log.show_message('r %r n %r' % (r, n))
+                    system_log.show_message('wc %r' % wc)
+                    system_log.show_message('lc %r' % lc)
 
             system_log.show_message(u'先手：挑小的出')
             return max_target
@@ -124,11 +128,11 @@ class ChunTingsBot(BaseBot):
         columns = info.players[info.me].hand.columns
 
         if not info.table.exchanged and info.table.n_game % 4 != 0:
-            pass_card = []
+            pass_card = set()
 
             topS = filter(lambda x: x >= 12, list(my_hand[my_hand['S'] > 0].index))
             for s in topS:
-                pass_card.append('%sS' % INT_TO_RANK[s])
+                pass_card.add('%sS' % INT_TO_RANK[s])
                 system_log.show_message(u'換掉黑桃大牌 %r' % pass_card)
 
             if len(pass_card) != 3:
@@ -138,19 +142,24 @@ class ChunTingsBot(BaseBot):
                         continue
                     if count <= 3 - len(pass_card):
                         for rank in list(my_hand[my_hand[columns[i]] > 0].index):
-                            pass_card.append('%s%s' % (INT_TO_RANK[rank], columns[i]))
-                            system_log.show_message(u'除了黑桃，如果剛好可以缺門就拼缺門' % pass_card)
+                            pass_card.add('%s%s' % (INT_TO_RANK[rank], columns[i]))
+                            system_log.show_message(u'除了黑桃，如果剛好可以缺門就拼缺門 %r' % pass_card)
 
             if len(pass_card) != 3:
                 for suit in ['C', 'H', 'D']:
                     tops = filter(lambda x: x >= 12, sorted(list(my_hand[my_hand[suit] > 0].index), reverse=True))
                     for s in tops:
                         if len(pass_card) < 3:
-                            pass_card.append('%sS' % INT_TO_RANK[s])
-                            system_log.show_message(u'如果還沒滿，換掉Ｑ以上的大牌' % pass_card)
+                            pass_card.add('%sS' % INT_TO_RANK[s])
+                            system_log.show_message(u'如果還沒滿，換掉Ｑ以上的大牌 %r' % pass_card)
+
+            if len(pass_card) != 3:
+                system_log.show_message(u'隨便啦')
+                for _ in range(3 - len(pass_card)):
+                    pass_card.add(random.choice(info.candidate))
 
             system_log.show_message('pass_card %r' % pass_card)
-            return pass_card
+            return list(pass_card)
 
         elif not info.table.finish_expose:
             # TODO
@@ -198,22 +207,13 @@ class ChunTingsBot(BaseBot):
 
             if pos == LAST:
                 max_rank = info.get_board_max()
-                if info.get_board_score() != 0:
-                    max_safe = 2
-                    for r,s in info.candidate:
-                        r = RANK_TO_INT[r]
-                        if s == info.table.first_draw[1] and r < max_rank and r >= max_safe:
-                            max_safe = r
-                    system_log.show_message(u'最後手：場上有分，挑小的出')
-                    return '%s%s' % (INT_TO_RANK[max_safe], info.table.first_draw[1])
-                else:
-                    max_rank = 2
-                    for r,s in info.candidate:
-                        r = RANK_TO_INT[r]
-                        if s == info.table.first_draw[1] and r >= max_rank:
-                            max_rank = r
-                    system_log.show_message(u'最後手：場上無分，挑大的出')
-                    return '%s%s' % (INT_TO_RANK[max_rank], info.table.first_draw[1])
+                max_safe = 2
+                for r,s in info.candidate:
+                    r = RANK_TO_INT[r]
+                    if s == info.table.first_draw[1] and r < max_rank and r >= max_safe:
+                        max_safe = r
+                system_log.show_message(u'最後手：挑安全牌中最大的出')
+                return '%s%s' % (INT_TO_RANK[max_safe], info.table.first_draw[1])
 
             pick_card = info.get_possiable_min(3 - pos)
 
@@ -263,7 +263,7 @@ class RuleBot(TrendConnector):
 
     def pass_cards(self, data):
         self.info.pass_to = data['receiver']
-        self.info.candidate = data['self']['candidateCards']
+        self.info.candidate = data['self']['cards']
         return self.bot.declare_action(self.info)
 
     def receive_opponent_cards(self, data):
